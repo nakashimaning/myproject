956e0f7228fe27431248fe0a5988f5a341d434ac
Chihning <>
2019-05-26 15:01:25 +0800

    Initial commit

---
+++ src/main/java/com/pratice/Person.java
@@ -0,0 +1,7 @@
+package com.pratice;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/com/pratice/Hello.kt
@@ -0,0 +1,18 @@
+package com.pratice
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+
+//    Human().hello() //產生物件並呼叫方法
+
+//    可以用val var來定義物件
+//    val(value)代表當我定義完這個物件之後就不能再改變他的值
+    val h = Human() //建構一個Human物件
+    h.hello()
+}
+
+class Human {   //直接設計類別,在同個package下,名稱不可以有跟java檔裡重複,否則會衝突
+    fun hello() { //不回傳任何方法可以不寫void
+        println("Hello kotlin") //可以加分號也可以不加
+    }
+}
